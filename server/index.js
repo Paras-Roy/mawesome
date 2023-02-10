cd820358fedb4638587aebfe4f452cc9f669d87b
Paras Roy <93031750+Paras-Roy@users.noreply.github.com>
2023-02-10 21:45:13 +0530

    api testing

--- server/index.js
+++ server/index.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 5000;
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+//middleware
+const cors = require('cors');
+app.use(cors());
+
 app.listen(port);
 
 //default route
